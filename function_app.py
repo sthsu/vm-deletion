ad0564208a178a1f04fb6c8fa0d10fa2bf2639a5
Sabrina Hsu <tihsua@tsmc.com>
2024-07-22 17:52:40 +0800

    update trigger time

--- function_app.py
+++ function_app.py
@@ -1,30 +1,14 @@
-# import logging
-# import azure.functions as func
-
-# app = func.FunctionApp()
-
-# @app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
-#               use_monitor=False) 
-# def timer_trigger(myTimer: func.TimerRequest) -> None:
-#     if myTimer.past_due:
-#         logging.info('The timer is past due!')
-
-#     logging.info('Python timer trigger function executed.')
-
-
 import logging,os,json
 import azure.functions as func
 from service.managed_bastion.cleanup_vms import CleanupVmsFunction
 
-from azure.identity import DefaultAzureCredential
-from azure.mgmt.compute import ComputeManagementClient
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = func.FunctionApp()
 
 @app.function_name(name="CleanupVMs")
-@app.schedule(schedule="0 0 * * * *", arg_name="mytimer", run_on_startup=True, use_monitor=False)
+@app.schedule(schedule="* */10 * * * *", arg_name="mytimer", run_on_startup=True, use_monitor=True)
 def main(mytimer: func.TimerRequest):
     logging.info('Azure Function timer trigger executed at: %s', mytimer.past_due)
     try:
@@ -52,26 +36,3 @@ def main(mytimer: func.TimerRequest):
         logger.error(f"Failed to import module: {str(e)}")        
     except Exception as e:
         logger.error("An error occurred: %s", str(e))
-
-# @app.function_name(name="CleanupVMs")
-# @app.schedule(schedule="0 0 * * * *", arg_name="mytimer", run_on_startup=True, use_monitor=False)
-# def main(mytimer: func.TimerRequest):
-#     logging.info('Azure Function timer trigger executed at: %s', mytimer.past_due)
-#     try:
-#         subscription_id = os.environ.get("SUBSCRIPTION_ID")
-#         resource_group_list_str = os.environ.get("RESOURCE_GROUP_LIST")
-        
-#         credentials = DefaultAzureCredential()
-#         compute_client = ComputeManagementClient(credentials,subscription_id)
-
-#         vms = compute_client.virtual_machines.list("1-9a6b55b8-playground-sandbox")
-#         for vm in vms:
-#             logging.warning(f'Deleting VM: {vm.name}')
-#             async_vm_delete=compute_client.virtual_machines.begin_delete("1-9a6b55b8-playground-sandbox",vm.name)
-#             async_vm_delete.result()
-      
-#         logger.info("Cleanup VMs function executed successfully")
-#     except ImportError as e:
-#         logger.error(f"Failed to import module: {str(e)}")        
-#     except Exception as e:
-#         logger.error("An error occurred: %s", str(e))
